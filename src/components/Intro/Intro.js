c3aa8a642131f9e929b85d5f8076b9e29e6f9e8e
behrooz <behrooz.zendedel1992@gmail.com>
2022-05-05 11:06:20 +0430

    add image png to intro section

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -1,5 +1,16 @@
 import React from "react";
 import "./Intro.css";
+import Github from "../../img/github.png";
+import linkdin from "../../img/linkdin.png";
+import instagram from "../../img/instagram.png";
+import Crown from "../../img/1.png";
+import Glass from "../../img/2.png";
+import intro from "../../img/intro.png";
+import FloatingDiv from "../FloatingDiv/FloatingDiv";
+import Float from "../../img/float.png";
+import Float2 from "../../img/float2.png";
+import imoji from "../../img/imoji.png";
+
 function Intro() {
   return (
     <div className="intro">
@@ -13,8 +24,42 @@ function Intro() {
           </span>
         </div>
         <button className="button i-button">Hire me</button>
+
+        <div className="i-icons">
+          <img src={Github} alt="" />
+          <img src={linkdin} alt="" />
+          <img src={instagram} alt="" />
+        </div>
       </div>
-      <div className="i-right">i am right side</div>
+      <div className="i-right">
+        <img src={Crown} alt="" />
+        <img src={Glass} alt="" />
+        <img src={intro} alt="" />
+        <img src={imoji} alt="" />
+        <div style={{ top: "-4%", left: "68%" }}>
+          <FloatingDiv image={Float} text1="web" text2="Developer" />
+        </div>
+        <div style={{ top: "18rem", left: "0rem" }}>
+          <FloatingDiv image={Float2} text1="tools" text2="desginer" />
+        </div>
+        {/* blur divs */}
+        <div
+          className="blur"
+          style={{
+            background: "rgb(238 210 255)",
+          }}
+        ></div>
+        <div
+          className="blur"
+          style={{
+            background: "#c1f5ff",
+            top: "17rem",
+            width: "21rem",
+            height: "11rem",
+            left: "-9rem",
+          }}
+        ></div>
+      </div>
     </div>
   );
 }
